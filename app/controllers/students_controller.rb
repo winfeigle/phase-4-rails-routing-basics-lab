09c21cab19024048d15a150fba6cc3af68831339
Win Feigle <win.feigle@gmail.com>
2023-02-08 07:51:11 -0700

    commit tree 357c82977c964ea2ce51b39733641ee4e1f117f4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,22 @@
 class StudentsController < ApplicationController
+
     def index
-        students = Student.all 
+        students = Student.all
         render json: students
     end
 
     def grades
-        students = Student.all.order(grade: :desc)
-        render json: students
+        render json: order_grades
     end
 
     def highest_grade
-        highest_grade = Student.all.order(grade: :desc).first
-        render json: highest_grade
+        render json: order_grades.first
+    end
+
+    private
+
+    def order_grades
+        students_grades = Student.all.order("grade").reverse()
     end
 
 end
